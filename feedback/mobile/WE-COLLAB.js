0817bfdf9ad97db48b1c50a08aa4efea11f2a15d
Giovanni Toffoli <toffoli@linkroma.it>
2022-08-21 00:05:16 +0200

    Added tentative DroidScript code for android app.

--- feedback/mobile/WE-COLLAB.js
+++ feedback/mobile/WE-COLLAB.js
@@ -1,40 +1,77 @@
 
+app.LoadScript("spinner.js");
+app.menu = [
+    {text: 'toggle', fun: toggleScreen},
+    {text: 'exit', fun: exitApp},
+];
+
 //Called when application is started.
 function OnStart()
 {
-  //Create a layout with objects vertically centered.
-  layVert = app.CreateLayout( "linear", "VTop,FillXY" );  
-  layVert.SetBackGradient("#ff00ff00","#ff000088");
 
-    //Create a header and add it to the layout.
-    layHeader = app.CreateLayout( "Absolute", "Vertical");
-    layVert.AddChild(layHeader);
+  //Create a root layout with objects vertically centered.
+  lay_0 = app.CreateLayout( "linear", "VTop,FillXY" );
+
+
+  //Create a header and add it to the root.
+  layHeader = app.CreateLayout( "Absolute", "Vertical");
+  lay_0.AddChild(layHeader);
 
     //Create a text and add it to the header
     txt1 = app.CreateText("WE-COLLAB Feedback",1,0.1,"VCenter,Center");
-    txt1.SetPosition(0,0,1,0.10);
+    // txt1.SetPosition(0,0,1,0.10);
+    txt1.SetPosition(0.18,0,0.82,0.10);
     txt1.SetPadding(0,0.01,0,0);
     txt1.SetTextSize(20);
     txt1.SetBackground("/Sys/Img/BlueBack.jpg");
     layHeader.AddChild(txt1);
-
+/*
     btn1 = app.CreateButton("[fa-power-off]",-1,-1,"FontAwesome");
     btn1.SetPosition(0,0,0.18,0.10);
     btn1.SetOnTouch( exitApp );
     layHeader.AddChild(btn1);
-
     btn2 = app.CreateButton("[fa-bars]",-1,-1,"FontAwesome");
-    btn2.SetPosition(0.82,0,0.18,0.10);
+    btn2.SetPosition(0,0,0.18,0.10);
     layHeader.AddChild(btn2);
+*/
+
+    list = app.menu;
+    title = 'M';
+    width = 0.5;
+    height = 0.05;
+    options = "VCenter,Center";
+    spinner = new Spinner(list, title, width, height, options);
+    spinner.onTouch = function()
+    {
+      app.ShowPopup(this.GetText());
+
+    }
+    spinner.SetPosition(0,0,0.18,0.10);
+    layHeader.AddChild(spinner);
+
+  //Create a layout with objects vertically centered.
+  lay_1 = app.CreateLayout( "linear", "VTop,FillXY" );
+  app.lay_1 = lay_1;
+  lay_0.AddChild(lay_1);
+  app.layer = lay_1;
 
     //Create an image and add it to the layout.
     img = app.CreateImage( "Img/attention.jpg", 1 );
-    layVert.AddChild( img );
+    lay_1.AddChild( img );
+
+    web = app.CreateWebView( 1.0, 0.5 );
+    web.LoadUrl( "https://www.commonspaces.eu?embed=true" );
+    lay_1.AddChild(web);
+
+  //Create a layout with objects vertically centered.
+  lay_2 = app.CreateLayout( "linear", "VTop,FillXY" );
+  app.lay_2 = lay_2;
+  lay_0.AddChild(lay_2);
 
   //Add a 3x3 button matrix 
 
     layHorizA = app.CreateLayout( "Linear", "Horizontal" );
-    layVert.AddChild( layHorizA );
+    lay_2.AddChild( layHorizA );
 
     btnA1 = app.CreateButton( "go on!", 0.33, 0.1 );
     btnA1.SetOnTouch( touchButton );
@@ -47,7 +84,7 @@ function OnStart()
     layHorizA.AddChild( btnA3 );
 
     layHorizB = app.CreateLayout( "Linear", "Horizontal" );
-    layVert.AddChild( layHorizB );
+    lay_2.AddChild( layHorizB );
 
     btnB1 = app.CreateButton( "pause", 0.33, 0.1 );
     btnB1.SetOnTouch( touchButton );
@@ -60,7 +97,7 @@ function OnStart()
     layHorizB.AddChild( btnB3 );
 
     layHorizC = app.CreateLayout( "Linear", "Horizontal" );
-    layVert.AddChild( layHorizC );
+    lay_2.AddChild( layHorizC );
 
     btnC1 = app.CreateButton( "context", 0.33, 0.1 );
     btnC1.SetOnTouch( touchButton );
@@ -72,12 +109,19 @@ function OnStart()
     btnC3.SetOnTouch( touchButton );
     layHorizC.AddChild( btnC3 );
 
-    web = app.CreateWebView( 1.0, 0.3 );
-    web.LoadUrl( "https://www.commonspaces.eu?embed=true" );
-    layVert.AddChild(web);
-
   //Add layout to app.  
-  app.AddLayout( layVert );
+  app.AddLayout( lay_0 );
+}
+
+function toggleScreen() {
+    if (app.lay_1.IsVisible()) {
+      app.lay_1.Gone();
+      app.lay_2.Show();
+    }
+    else {
+      app.lay_2.Gone();
+      app.lay_1.Show();
+    }
 }
 
 function exitApp()
---
+++ feedback/mobile/spinner.js
@@ -0,0 +1,69 @@
+/* adapted from https://github.com/musaaj/Droidscript-Custom-Spinner */
+
+function Spinner(list, title, width, height, options)
+{
+  this.list = list;
+  this.title  = title;
+  this.width = width;
+  this.height = height;
+  this.options  = options;
+  
+  this.toggle = function ()
+  {
+    this.left = this.GetLeft(  );
+    this.top = this.GetTop(  );
+    
+    if (this.collapsed )
+    {
+      this.lay = app.CreateLayout( "absolute", "TouchThrough" );
+      this.lay.SetPosition(1,1,1,1);
+      this.lay.SetPadding( 0,0.025,0,0 );
+      this.lay.SetOnTouchDown( function()
+      {
+        app.DestroyLayout( this );
+      });
+      
+      this.scrollLay = app.CreateLayout( "absolute", "TouchThrough" );
+      this.scrollLay.SetPosition( this.left, this.top, -1, -1);
+      this.scrollLay.SetPadding( 0.01, 0.01, 0.01, 0.01 );
+      this.lay.AddChild( this.scrollLay );
+      
+      this.scroll = app.CreateScroller(-1,-1);
+      this.scroll.SetBackColor( "#05000000" );
+      this.scroll.SetPadding( 0.005, 0, 0.005, 0.005);
+      this.scrollLay.AddChild( this.scroll );
+      
+      this.tray  = app.CreateLayout( "Linear", "TouchThrough" );
+      this.tray.SetBackColor( "#000000" );
+      this.scroll.AddChild(this.tray );
+      
+      for ( i in this.list)
+      {
+        this.item = app.CreateText(this.list[i].text,this.width,this.height,this.options+"FillX");
+        this.item.SetOnTouchDown( this.list[i].fun);
+        this.item.SetPadding( 0.01,0.01,0.01,0.01 );
+        this.item.SetMargins( 0, 0, 0, 0.002);
+        this.tray.AddChild( this.item );
+      }
+      
+      app.AddLayout( this.lay );
+      this.collapsed = false;
+    }
+    else
+    {
+      this.collapsed  = true;
+      app.DestroyLayout( this.lay );
+    }
+  }
+
+  this.titleTxt = app.CreateText( this.title, this.width, this.height, this.options);
+  this.titleTxt.list = this.list;
+  this.titleTxt.title = this.title;
+  this.titleTxt.width = this.width;
+  this.titleTxt.height = this.height;
+  this.titleTxt.options = this.options;
+  this.titleTxt.collapsed = true;
+  this.titleTxt.onTouch  = null;
+  this.titleTxt.SetOnTouchDown( this.toggle);
+  return this.titleTxt;
+}
