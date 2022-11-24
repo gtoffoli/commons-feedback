b1e388b3323d12c39e66f12ec4032c51e8075fe8
mrmanco <manco@linkroma.it>
2022-11-24 11:00:05 +0100

    updated cover.js

--- feedback/mobile/cover.js
+++ feedback/mobile/cover.js
@@ -4,11 +4,11 @@ function coverScreen()
   this.lay = app.CreateLayout( "linear", "VTop,FillXY" );
 
   //Create an image and add it to the layout.
-  img = app.CreateImage( "Img/WE-COLLAB.png", 1, 0.46 );
+  img = app.CreateImage( "Img/We-collab.png", 1, 0.50 );
   this.lay.AddChild( img );
 
   //Create another image and add it to the layout.
-  img = app.CreateImage( "Img/lecture-feedback.png", 1, 0.46 );
+  img = app.CreateImage( "Img/lecture-feedback.png", 1, 0.42 );
   this.lay.AddChild( img );
 
   return this.lay;
