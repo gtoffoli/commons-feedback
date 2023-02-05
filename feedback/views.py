459bbce658c6a31f1ea0e00c444edf7451950f84
Giovanni Toffoli <toffoli@linkroma.it>
2023-02-05 22:10:58 +0100

    Improved the dashboard and enforced time span of events.

--- feedback/views.py
+++ feedback/views.py
@@ -5,13 +5,16 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
+import django.utils.translation
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from schedule.models import Event, CalendarRelation
+from datatrans.utils import get_current_language
 from commons.models import Project
 from commons.tracking import track_action
 from commons.utils import unshuffle_integers
 from feedback.producers import reaction_item_producer, chat_item_producer
+# from future.backports.test.pystone import FALSE
 
 
 def room_select(request):
@@ -21,6 +24,26 @@ def chat_room(request, room_name):
     return render(request, 'feedback/chat_room.html', {
         'room_name': room_name
     })
+
+def word_array():
+    def _(x):
+        y = django.utils.translation.gettext_lazy(x)
+        return str(y)
+    array = [
+      { 'text': "louder", 'label': _("louder"), 'help': _("speack louder"), },
+      { 'text': "slower", 'label': _("slower"), 'help': _("speak slower"), },
+      { 'text': "pause", 'label': _("pause"), 'help': _("pause a little"), },
+      { 'text': "go-on", 'label': _("go on"), 'help': _("restart or proceed"), 'link': "https://google.com" },
+      { 'text': "repeat", 'label': _("repeat"), 'help': _("repeat last sentences"), },
+      { 'text': "explain", 'label': _("explain"), 'help': _("explain better"), },
+      { 'text': "context", 'label': _("context"), 'help': _("provide context information"), },
+      { 'text': "example", 'label': _("example"), 'help': _("provide an example"), },
+      { 'text': "recap", 'label': _("recap"), 'help': _("recap last topic"), }
+    ]
+    for item in array:
+        item['last'] = False
+        item['weight'] = 1
+    return array
 
 def feedback_dashboard(request, event_code):
     event_id, user_id = unshuffle_integers(event_code)
@@ -34,6 +57,8 @@ def feedback_dashboard(request, event_code):
             event = events[0]
             calendar = event.calendar
             relations = CalendarRelation.objects.filter(calendar=calendar)
+            now = timezone.now()
+            not_running = now < event.start or now > event.end
             project_id = relations[0].object_id
             project = Project.objects.get(id=project_id)
             return render(request, 'feedback/feedback_dashboard.html', {
@@ -42,6 +67,8 @@ def feedback_dashboard(request, event_code):
                 'event_name': event_name,
                 'event_title': event.title,
                 'project_name': project.name,
+                'not_running': not_running,
+                'word_array': json.dumps(word_array()),
                 'VUE': True,
             })
     return render(request, 'feedback/feedback_dashboard.html', {
@@ -91,6 +118,8 @@ def validate_event(request):
     data['error'] = error
     return JsonResponse(data)
 
+time_tolerance = 10 # how many minutes the event may exceed the nominal duration 
+
 @csrf_exempt
 def reaction_message(request):
     """ Gets and processes real-time user reaction from mobile app and emit xAPI statement.
@@ -127,20 +156,19 @@ def reaction_message(request):
             now = timezone.now()
             if now < event.start or now > event.end:
                 data['warning'] = _('event is not running')
-            calendar = event.calendar
-            relations = CalendarRelation.objects.filter(calendar=calendar)
-            project_id = relations[0].object_id
-            project = Project.objects.get(id=project_id)
-            message = '{}-{}: {}'.format(str(now)[11:19], user_name, reaction)
-            if project.is_member(user):
-                # track_action(request, user, verb, event, target=project)
-                track_action(request, user, verb, event, target=project, response=reaction)
             else:
-                data['warning'] = _('user is not member of community/project')
-            # push line to all raw feedback visualizers
-            reaction_item_producer(group_name, message)
+                calendar = event.calendar
+                relations = CalendarRelation.objects.filter(calendar=calendar)
+                project_id = relations[0].object_id
+                project = Project.objects.get(id=project_id)
+                message = '{}-{}: {}'.format(str(now)[11:19], user_name, reaction)
+                if not project.is_member(user):
+                    data['warning'] = _('user is not member of community/project')
+                else:
+                    track_action(request, user, verb, event, target=project, response=reaction)
+                    # push line to all raw feedback visualizers
+                    reaction_item_producer(group_name, message)
     return JsonResponse(data)
-
 
 @csrf_exempt
 def chat_message(request):
@@ -179,17 +207,18 @@ def chat_message(request):
             now = timezone.now()
             if now < event.start or now > event.end:
                 data['warning'] = _('event is not running')
-            calendar = event.calendar
-            relations = CalendarRelation.objects.filter(calendar=calendar)
-            project_id = relations[0].object_id
-            project = Project.objects.get(id=project_id)
-            message = '{}-{}: {}'.format(str(now)[11:19], user_name, message)
-            if project.is_member(user):
-                track_action(request, user, verb, event, target=project)
             else:
-                data['warning'] = _('user is not member of community/project')
-            # push line to all raw feedback visualizers
-            chat_item_producer(group_name, message)
+                calendar = event.calendar
+                relations = CalendarRelation.objects.filter(calendar=calendar)
+                project_id = relations[0].object_id
+                project = Project.objects.get(id=project_id)
+                message = '{}-{}: {}'.format(str(now)[11:19], user_name, message)
+                if not project.is_member(user):
+                    data['warning'] = _('user is not member of community/project')
+                else:
+                    track_action(request, user, verb, event, target=project)
+                    # push line to all raw feedback visualizers
+                    chat_item_producer(group_name, message)
     return JsonResponse(data)
 
 # https://stackoverflow.com/questions/70159895/django-send-events-via-websocket-to-all-clients
